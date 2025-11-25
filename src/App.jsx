fcd57df742651a1c32c0f2c73fd73be5cb6ef843
aniruddhapai27 <aniruddhaanantpai@gmail.com>
2025-11-25 21:41:49 +0530

    Basic react setup completed

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-9xl font-bold underline ">Major Project</h1>
+    </div>
+  );
+}
+
+export default App;
